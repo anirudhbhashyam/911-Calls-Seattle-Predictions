13014080358c10cfad14e92ca985b64982a86f63
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2022-02-07 06:45:17 +0100

    Basic LSTM and GBR

--- src/train.py
+++ src/train.py
@@ -2,29 +2,51 @@ import os
 
 import pandas as pd
 import numpy as np
+
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, cross_val_score, KFold, RandomizedSearchCV
 
 import utility as ut
 
-DATA_PATH = os.path.abspath("../data/train-test")
-DATA_TRAIN = "final_data_mean"
-DATA_TEST = "final_data_mean_test"
+DATA_PATH = os.path.relpath("../data/train-test")
+DATA_TRAIN = "train_data"
+DATA_TEST = "test_data"
 DATA_EXT = "csv"
 LABEL = "calls"
 
-data = pd.read_csv(os.path.join(DATA_PATH, ".".join([DATA_TRAIN, DATA_EXT])), header = 0)
+train_data = pd.read_csv(os.path.join(DATA_PATH, ".".join([DATA_TRAIN, DATA_EXT])), header = 0)
 test_data = pd.read_csv(os.path.join(DATA_PATH, ".".join([DATA_TEST, DATA_EXT])), header = 0)
 
-Y = data.pop(LABEL)
+Y = train_data.pop(LABEL)
 Y_test = test_data.pop(LABEL)
 
-X_train, X_val, Y_train, Y_val = train_test_split(data, Y, shuffle = True, random_state = 43)
+X_train, X_val, Y_train, Y_val = train_test_split(train_data, Y, shuffle = True, random_state = 43)
 
-reg = GradientBoostingRegressor(n_iter_no_change = 5,
-                                validation_fraction = 0.2)
-reg.fit(X_train, Y_train)
+
+NUM_TRIALS = 10
+# Parameter space.
+p_grid = {"alpha": np.arange(0.01, 1, 0.1), 
+          "learning_rate": np.arange(0.01, 0.1, 0.02), 
+          "ccp_alpha": np.arange(0, 2, 0.1), 
+          "min_samples_leaf": range(1, 30), 
+          "n_estimators": range(100, 500, 100)}
+
+# Arrays to store scores.
+non_nested_scores = np.zeros(NUM_TRIALS)
+nested_scores = np.zeros(NUM_TRIALS)
+
+inner_cv = KFold(n_splits = 4, shuffle = True, random_state = 42) 
+# outer_cv = KFold(n_splits = 4, shuffle = True, random_state = 43)
+
+# Unnested parameter search and scoring.
+reg = RandomizedSearchCV(estimator = GradientBoostingRegressor(), 
+                         param_distributions = p_grid, 
+                         cv = inner_cv, 
+                         n_iter = 100)
+reg.fit(train_data, Y)
+# non_nested_scores[i] = clf.best_score_
 
 predictions = reg.predict(test_data)
 
--- src/utility.py
+++ src/utility.py
@@ -1,18 +1,20 @@
 import numpy as np
+import pandas as pd
+from typing import List
 
 def lossy_error(predictions: np.array, labels: np.array, tolerance: float = 2) -> float:
-    """
-    Compares the `predictions` and `labels` with a `tolerance.`
-    
-    Parameters
-    ----------
-    predictions:
+	"""
+	Compares the `predictions` and `labels` with a `tolerance.`
+	
+	Parameters
+	----------
+	predictions :
 		An array of predicted values.
   
-	labels:
+	labels :
 		An array of actual values.
   
-	tolerance:
+	tolerance :
 		A number within which the error is acceptable.
   
 	Returns
@@ -24,10 +26,42 @@ def lossy_error(predictions: np.array, labels: np.array, tolerance: float = 2) -
 	------
 	AttributeError:
 		If predictions and labels are not of the same length.
-    """
-    if predictions.shape[0] != labels.shape[0]:
-        raise AttributeError(f"The shapes of prediction and labels should match in dim 0.")
-    
-    return sum(1 for pred, label in zip(predictions, labels) if abs(pred - label) <= tolerance) / len(predictions)
-    
-    
+	"""
+	if predictions.shape[0] != labels.shape[0]:
+		raise AttributeError(f"The shapes of prediction and labels should match in dim 0.")
+	
+	return sum(1 for pred, label in zip(predictions, labels) if abs(pred - label) <= tolerance) / len(predictions)
+
+def scale(data: pd.DataFrame, cols: List[str] = None, type: str = "mean") -> pd.DataFrame:
+	"""
+	Normalises columns of `data` using the method `type`.
+	
+	Parameters
+	----------
+	data :
+		The dataframe who's columns need to be normalised.
+  
+	cols : 
+		The columnss to be normalised. By deafult is `None` which mean normalises the entire dataframe.
+	
+	type : 
+		Acceptable methods are *mean* and *min-max*.
+
+	Returns
+	-------
+	pd.DataFrame:
+		A normalised version of the input dataframe.
+	"""
+	norm_data = data.copy()
+ 
+	if cols is None:
+		return (norm_data - norm_data.mean()) / norm_data.std()
+
+	if type == "mean":
+		norm_data[cols] = (norm_data[cols] - norm_data[cols].mean()) / norm_data[cols].std()
+
+	if type == "min-max":
+		norm_data[cols] = (norm_data[cols] - norm_data[cols].min()) / (norm_data[cols].max() - norm_data[cols].min())
+  
+	return norm_data
+	
